caa462008658def2678d9f85fa9c2076e0d45f80
startpeeco <134424987+startpeeco@users.noreply.github.com>
2024-02-06 13:58:52 -0300

    Commit from TeleportHQ

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = (props) => {
+  return (
+    <>
+      <div className="home-container">
+        <Head>
+          <title>Achados Live</title>
+          <meta property="og:title" content="Achados Live" />
+        </Head>
+      </div>
+      <style jsx>
+        {`
+          .home-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+            justify-content: center;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Home
