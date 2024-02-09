c70830e05ce2c2d731a17895cdc00dfdb4f930b0
startpeeco <134424987+startpeeco@users.noreply.github.com>
2024-02-09 11:04:08 -0300

    Commit from TeleportHQ

---
+++ pages/consultoria.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Consultoria = (props) => {
+  return (
+    <>
+      <div className="consultoria-container">
+        <Head>
+          <title>Consultoria - Achados Live</title>
+          <meta property="og:title" content="Consultoria - Achados Live" />
+        </Head>
+        <button
+          type="button"
+          autoFocus="true"
+          className="consultoria-button button"
+        >
+          Iniciar conversa
+        </button>
+      </div>
+      <style jsx>
+        {`
+          .consultoria-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+            justify-content: center;
+          }
+          .consultoria-button {
+            font-size: 40px;
+          }
+          @media (max-width: 767px) {
+            .consultoria-button {
+              font-size: 40px;
+            }
+          }
+          @media (max-width: 479px) {
+            .consultoria-button {
+              font-size: 40px;
+              border-width: 4px;
+              border-radius: var(--dl-radius-radius-radius8);
+              background-color: rgb(65, 239, 106);
+            }
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Consultoria
